09796b2f28f7c5e48133ec4c9e8cdf3682999a36
Иван Лукьянский <ivanlukanskiy@MacBook-Air-Ivan.local>
2025-06-24 04:09:08 +0300

    без победы и поражения и ничьи, сырой варик, но поиграть можно ебана рот

--- help/help.go
+++ help/help.go
@@ -8,10 +8,17 @@ import (
 // func input();
 
 func Graph(matr [3][3]int) {
+	fmt.Printf("\nENTER A LINE(0-2) AND COLUMN(0-2)((USE ENTER :-) )) THEN MAKE UR CHOICE (X OR 0)!\n\n")
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
-			fmt.Printf(" %d ", matr[i][j])
-			if j < 3-1 {
+			if matr[i][j] == 1 {
+				fmt.Print(" X ")
+			} else if matr[i][j] == 2 {
+				fmt.Print(" 0 ")
+			} else {
+				fmt.Print("   ")
+			}
+			if j < 2 {
 				fmt.Print("|")
 			}
 		}
@@ -28,10 +35,23 @@ func Graph(matr [3][3]int) {
 	}
 }
 
-func Input(matr [3][3]int) {
+func Input() (line int, col int, choice string) {
 
+	fmt.Scanln(&col)
+	fmt.Scanln(&line)
+	fmt.Scan(&choice)
+	return
 }
 
-func Update(matr [3][3]int) {
-
+func Update(matr *[3][3]int, line int, col int, player int) bool {
+	if line < 0 || line > 2 || col < 0 || col > 2 {
+		fmt.Println("ERROR. ENTER A VALID COORDS")
+		return false
+	}
+	if matr[col][line] != 0 {
+		fmt.Printf("EROR// CELL IS ALREADY FULL!\n")
+		return false
+	}
+	matr[line][col] = player
+	return true
 }
--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import(
+import (
 	"blyat/help"
+	"fmt"
 )
+
 func main() {
-	var matr [3][3] int
+	var matr [3][3]int
 	help.Graph(matr)
+	for {
+		col, line, choice := help.Input()
+		var player int
+		if choice == "X" {
+			player = 1
+		} else if choice == "0" {
+			player = 2
+		} else {
+			fmt.Println("ERROR")
+			return
+		}
+		success := help.Update(&matr, line, col, player)
+		if !success {
+			fmt.Println("NOPE")
+			return
+		}
+		help.Graph(matr)
+	}
 }
